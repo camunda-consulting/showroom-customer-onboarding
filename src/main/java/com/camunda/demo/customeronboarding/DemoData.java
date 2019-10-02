3c1a7a6891cc31264bf5698c322ad6710a0609e9
Felix Anhalt <felix.anhalt@camunda.com>
2019-10-02 09:15:49 +0200

    move css and js files to one central point

--- src/main/java/com/camunda/demo/customeronboarding/DemoData.java
+++ src/main/java/com/camunda/demo/customeronboarding/DemoData.java
@@ -468,7 +468,7 @@ public class DemoData {
         LOGGER.info("----                                                             ----");
         LOGGER.info("----                                                             ----");
         long begin = System.currentTimeMillis();
-        SimulationExecutor.execute(DateTime.now().minusMonths(12).toDate(), DateTime.now().toDate());
+        //SimulationExecutor.execute(DateTime.now().minusMonths(12).toDate(), DateTime.now().toDate());
         long end = System.currentTimeMillis();
         LOGGER.info("----                                                     ----");
         LOGGER.info("----                                                     ----");
