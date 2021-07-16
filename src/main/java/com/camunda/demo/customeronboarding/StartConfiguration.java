cc85df84b77738e9102b3848ca1cbd8a8c41e40a
Kristin Polenz <kristin.polenz@camunda.com>
2021-07-16 22:37:21 +0200

    impr(DemoData): use conditional beans
    - load beans 'SimulatorPlugin' and 'PayloadGenerator' only if generation
    of demo data activate
    - ToDo:
    -- use only one property to decide what should be happen during the
    start of the customer-onboarding application
    -- delete 'else'-part in method 'setupEnvironmentForDemo'
    -- delete first 'Entrypoint' definition in Dockerfile
    -- check line 'chown app:app /${DEPLOYMENT_ARTIFACT}' in Dockerfile ->
    compare with Dockerfile in dataupload-app
    -- think about the property usage to decide which way should be used in the Dockerfile
    
    related to REV-1567

--- src/main/java/com/camunda/demo/customeronboarding/StartConfiguration.java
+++ src/main/java/com/camunda/demo/customeronboarding/StartConfiguration.java
@@ -1,6 +1,9 @@
 package com.camunda.demo.customeronboarding;
 
 import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,8 @@ import com.camunda.demo.sso.AutoLoginAuthenticationFilter;
 
 @Configuration
 public class StartConfiguration {
+
+  private final static Logger LOGGER = LoggerFactory.getLogger(StartConfiguration.class);
   
   @Bean(name="corporationMapper")
   public ExecutionListener getMapper () {
@@ -23,15 +28,25 @@ public class StartConfiguration {
   }
   
   @Bean
+  @ConditionalOnProperty(
+	  value="shouldsimulate",
+	  havingValue = "true",
+	  matchIfMissing = false)
   public SimulatorPlugin simulatorPlugin() {
+	  LOGGER.info("------LOAD SIMULATOR PLUGIN------");
         return new SimulatorPlugin();
   }
-  
+
   @Bean
+  @ConditionalOnProperty(
+	  value="shouldsimulate",
+	  havingValue = "true",
+	  matchIfMissing = false)
   public PayloadGenerator generator() {
+	  LOGGER.info("------LOAD PAYLOAD GENERATOR------");
         return new ContentGenerator();
   }
-  
+
   @Bean
   public FilterRegistrationBean<AutoLoginAuthenticationFilter> loggingFilter(){
       FilterRegistrationBean<AutoLoginAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
