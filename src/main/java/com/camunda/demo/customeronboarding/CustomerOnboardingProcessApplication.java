7269fc5ffd5e0bc3dbd247c66b70f9ae4aee5b2d
Falko Menge <falko.menge@camunda.com>
2021-08-17 17:50:41 +0200

    Switch from embedded Camunda engine to Zeebe client

--- src/main/java/com/camunda/demo/customeronboarding/CustomerOnboardingProcessApplication.java
+++ src/main/java/com/camunda/demo/customeronboarding/CustomerOnboardingProcessApplication.java
@@ -1,14 +1,16 @@
 package com.camunda.demo.customeronboarding;
 
-import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.beans.factory.annotation.Autowired;
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
-@EnableProcessApplication("customer-onboarding")
+@EnableZeebeClient
+@ZeebeDeployment(classPathResources = {"static/bpmn/customer_onboarding_en.bpmn", "static/bpmn/document_request_en.bpmn"})
 public class CustomerOnboardingProcessApplication {
 
   public static void main(String[] args) {
