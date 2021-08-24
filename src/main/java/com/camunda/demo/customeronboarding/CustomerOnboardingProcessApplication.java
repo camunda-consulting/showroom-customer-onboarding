2b92d60b8b96537e9aa331194002a6348448b4df
Falko Menge <falko.menge@camunda.com>
2021-08-24 22:36:20 +0200

    Remove unused imports

--- src/main/java/com/camunda/demo/customeronboarding/CustomerOnboardingProcessApplication.java
+++ src/main/java/com/camunda/demo/customeronboarding/CustomerOnboardingProcessApplication.java
@@ -1,10 +1,8 @@
 package com.camunda.demo.customeronboarding;
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
