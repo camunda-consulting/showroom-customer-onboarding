3426206f0a462fb85cc4808a0d6b79d25fef4b76
Ragnar Nevries <ragnar.nevries@camunda.com>
2021-09-02 15:45:14 -0600

    replace bash by java to react on SIGTERM etc

--- datatransfer.sh
+++ datatransfer.sh
@@ -10,7 +10,7 @@ then
   java ${JAVA_OPTS} -Xmx2048m ${DOCKER_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ./camunda-showcase-customer-onboarding.jar  -D && sleep 10 && node ./dataupload-app.js -D
 elif [ $mode = "run" ]
 then
-  node ./dataupload-app.js -D && sleep 10 && java ${JAVA_OPTS} ${DOCKER_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ./camunda-showcase-customer-onboarding.jar  -D
+  node ./dataupload-app.js -D && sleep 10 && exec java ${JAVA_OPTS} ${DOCKER_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ./camunda-showcase-customer-onboarding.jar  -D
 else
-  java ${JAVA_OPTS} -Xmx2048m ${DOCKER_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ./camunda-showcase-customer-onboarding.jar
+  exec java ${JAVA_OPTS} -Xmx2048m ${DOCKER_JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar ./camunda-showcase-customer-onboarding.jar
 fi
