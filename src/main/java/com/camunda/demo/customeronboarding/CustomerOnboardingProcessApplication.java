e5bdd43d935e2cda5b62d4486e2ee3e06a061b7c
rob2universe <robert.emsbach@gmail.com>
2023-09-04 14:59:52 +0800

    upgrade

--- src/main/java/com/camunda/demo/customeronboarding/CustomerOnboardingProcessApplication.java
+++ src/main/java/com/camunda/demo/customeronboarding/CustomerOnboardingProcessApplication.java
@@ -1,12 +1,10 @@
 package com.camunda.demo.customeronboarding;
 
-import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
 @Deployment(resources = {"static/bpmn/customer_onboarding_en.bpmn", "static/bpmn/document_request_en.bpmn", "static/bpmn/risk_check_en.dmn"})
 public class CustomerOnboardingProcessApplication {
 
